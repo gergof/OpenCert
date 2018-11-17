ab87cd232e97d36da5fdaf3d522cd19f2ae03b80
Fándly Gergő <gergo@systemtest.tk>
2018-11-17 14:19:24 +0200

    Working UI

--- src/script/login.js
+++ src/script/login.js
@@ -1,25 +1,27 @@
 import $ from "jquery";
 import NodeRSA from "node-rsa";
 import Modal from "./components/Modal.js";
+import {route, loadMessages} from "./global.js";
 
 export const toggleRemember=() => {
     $("#remember_checkbox").prop("checked", !$("#remember_checkbox").prop("checked"));
 }
 
 export const validateUsername=(el) => {
-    $(el).removeClass("checkinput__ok").removeClass("checkinput__error").addClass("checkinput__pending");
+    var cont=$(el).parent(".checkinput");
+    cont.removeClass("checkinput__ok").removeClass("checkinput__error").addClass("checkinput__pending");
     if($(el).val()!=""){
         $.ajax({
             url: "./modules/loader.php",
             method: "GET",
             data: {load: "login", sub: "register", username_available: $(el).val()}
         }).then((resp) => {
-            $(el).removeClass("checkinput__pending")
+            cont.removeClass("checkinput__pending")
             if(resp=="ok"){
-                $(el).addClass("checkinput__ok");
+                cont.addClass("checkinput__ok");
             }
             else{
-                $(el).addClass("checkinput__error");
+                cont.addClass("checkinput__error");
             }
         });
     }
@@ -30,6 +32,8 @@ export const generateKeypair=() => {
     key.generateKeyPair(4096);
 
     $("#rsa_public").val(key.exportKey("pkcs1-public-pem"));
+    $("#rsa_public").trigger("input");
+
     Modal({
         title: $("#lang_rsaPrivate").text(),
         content: $("#lang_rsaPrivateTooltip").text()+"<br/><br/>"+key.exportKey("pkcs1-private-pem").replace(/\n/g, "<br/>"),
@@ -39,31 +43,46 @@ export const generateKeypair=() => {
                 action: "close",
                 class: "button button__red",
                 icon: "exclamation-triangle",
-                label: $("#lang_rsaPrivateTooltip").text()
+                label: $("#lang_rsaPrivateWrotedown").text()
             }
         ]
     });
 };
 
 export const validatePublicKey=(el) => {
-    $(el).removeClass("checkinput__ok").removeClass("checkinput__error").addClass("checkinput__pending");
+    var cont=$(el).parent(".checkinput");
+    cont.removeClass("checkinput__ok").removeClass("checkinput__error").addClass("checkinput__pending");
+
     const key=new NodeRSA();
 
     try{
         key.importKey($(el).val());
         if(key.isPublic(true)){
-            $(el).removeClass("checkinput__pending").addClass("checkinput__ok");
+            cont.removeClass("checkinput__pending").addClass("checkinput__ok");
         }
         else{
-            $(el).removeClass("checkinput__pending").addClass("checkinput__error");
+            cont.removeClass("checkinput__pending").addClass("checkinput__error");
         }
     }
     catch(e){
-        $(el).removeClass("checkinput__pending").addClass("checkinput__ok");
+        cont.removeClass("checkinput__pending").addClass("checkinput__error");
     }
 };
 
 export const register=(e) => {
     e.preventDefault();
-    console.log($("#register_form").serialize());
+
+    $.ajax({
+        url: "./modules/loader.php?load=login&sub=register",
+        method: "POST",
+        data: {register: $("#register_form").serialize()}
+    }).then((resp) => {
+        if(resp=="ok"){
+            loadMessages();
+            route("login");
+        }
+        else{
+            loadMessages();
+        }
+    });
 };
